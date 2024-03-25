0a15bab0fc77b33e54c413f54a760d0e3fda399d
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-03-25 20:04:31 +0530

    bottom nav

--- App.tsx
+++ App.tsx
@@ -1,15 +1,58 @@
 import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
 import { PaperProvider } from 'react-native-paper';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import StackNavigation from './src/navigations/stack-navigation';
 
-const App = () => {
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StackNavigation />
-      </NavigationContainer>
-    </PaperProvider>
+    <SafeAreaView style={styles.container}>
+      <PaperProvider>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <NavigationContainer>
+          <StackNavigation />
+        </NavigationContainer>
+      </PaperProvider>
+    </SafeAreaView>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
 export default App;
--- src/navigations/bottom-navigation.tsx
+++ src/navigations/bottom-navigation.tsx
@@ -1,8 +1,12 @@
 import { StyleSheet } from 'react-native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { useTheme } from 'react-native-paper';
 import HomeScreen from '../screens/home-screen/home-screen';
+import TransactionsScreen from '../screens/transactions-screen/transactions-screen';
+import BudgetScreen from '../screens/budget-screen/budget-screen';
+import ProfileScreen from '../screens/profile-screen/profile-screen';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -23,6 +27,36 @@ const BottomNavigation = () => {
           tabBarLabel: 'Home',
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="home" color={color} size={22} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Transactions"
+        component={TransactionsScreen}
+        options={{
+          tabBarLabel: 'Transactions',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="arrow-redo" color={color} size={22} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Budget"
+        component={BudgetScreen}
+        options={{
+          tabBarLabel: 'Budget',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="chart-pie" color={color} size={22} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={22} />
           ),
         }}
       />
