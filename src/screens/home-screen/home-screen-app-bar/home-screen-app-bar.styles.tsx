9ffa73ad32019cd1712804702f867a0e5cbbcd2f
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-04-02 11:33:29 +0530

    app bar component

--- src/screens/home-screen/home-screen-app-bar/home-screen-app-bar.styles.tsx
+++ src/screens/home-screen/home-screen-app-bar/home-screen-app-bar.styles.tsx
@@ -1,5 +1,8 @@
+import { View } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import styled from 'styled-components';
+
+export const AppbarContainer = styled(View)``;
 
 export const AppbarHeader = styled(Appbar.Header)`
   background-color: 'transparent';
